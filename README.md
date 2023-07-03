e43f336e5eb95404f9629bebb36e8ddea856254b
masterz5398 <90090313+masterz5398@users.noreply.github.com>
2023-07-03 20:32:25 +0530

    major update

--- README.md
+++ README.md
@@ -7,36 +7,41 @@ This is a simple Python proxy server that adds CORS headers to M3U8 playlist fil
 To install and run the server, follow these steps:
 
 1. Clone the repository:
-
-```py
-git clone https://github.com/masterz5398/m3u8_proxy-cors.git
-```
-
+    ```bash
+    git clone https://github.com/masterz5398/m3u8_proxy-cors.git
+    ```
 2. Install dependencies:
     
-```py
-cd m3u8_proxy-cors
-pip install -r requirements.txt
-```
+    ```bash
+    cd m3u8_proxy-cors
+    pip install -r requirements.txt
+    ```
 3. Start the server:
-```py
-python main.py
-```
+    ```bash
+    python main.py
+    ```
 
-The server will start listening on port 5010 by default. You can change the port by setting the `port` variable in main.py
+The server will start listening on port 5010 by default. You can change the port by setting the `port` environment variable
+
+You may configure which domains can use this proxy by setting the `origins` environment variable, by default it will allow all origins for people who wish to use it just as the older version
+
+You may enable or disable the documentation which is available on the /docs path by setting `documentation` as 
+- `True` to enable
+- `False` to disable (**Default**)
+
+you may also change the documentation url by setting the `docs_url` and `redoc_url` environment variable, default value is '**_/docs_**' and '**_/redoc_**' by default
+- setting the value as '' _(empty string)_ will remove the documentation (useful in cases when you want only one documentation)
+- **Setting the documentation env variable as False will remove both the documentation and these env variables will have no effect** 
+- `docs_url` is for _swagger documentation_ and `redoc_url` is for _redoc documentation_
+
 
 ## Usage
 
 To use the server, simply replace the URL of the M3U8 playlist file in your application with the URL of the proxy server. For example, if your original URL was `https://example.com/video.m3u8`, you would replace it with `http://localhost:5010/cors?url=https://example.com/video.m3u8`.
 
-For m3u8s ending with #.mp4 you should add origin=aninin in query like ```http://localhost:5010/cors?url=https://example.com/video.m3u8&origin=aninin```
-
-
 To set custom headers for a request you can do ```http://localhost:5010/cors?url=https://example.com/video.m3u8&headers={"User-Agent":"Mozilla/5.0 ...","referer":"https://example.com",...}```
 
-don't put spaces between the headers(you can put it inside the quotes) you might get a 500 error, 
-
-do not encode the url or headers as we do not have a decoder for it
+don't put spaces between the headers (you can put it inside the quotes) you might get a 500 error,
 
 ## Deploying to Vercel
 You can quickly deploy this project to Vercel with the following button:
--- main.py
+++ main.py
@@ -1,145 +1,41 @@
-import json
-import aiohttp
-import multidict
+import os
 import uvicorn
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
-import secrets
+from cors import add_cors
 
-app = FastAPI()
-
-TOKENS = {}
-
-CharLIST = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-
-
-async def home(request: Request) -> Response:
-    return RedirectResponse("/docs")
-
-
-def NEW_ID(item):
-    id_len = 32
-    _id = ''
-    if item in list(TOKENS.values()):
-        for ite in TOKENS:
-            if TOKENS[ite] == item:
-                return ite
-    while True:
-        while len(_id) < id_len:
-            _id += secrets.choice(CharLIST)
-        if _id in TOKENS:
-            _id = ''
-        else:
-            TOKENS[_id] = item
-            return _id
+try:
+    enable_docs = bool(os.getenv("documentation", False))
+    docs_url = os.getenv('docs_url', '/docs') if enable_docs and os.getenv('docs_url', '/docs') else None
+    redoc_url = os.getenv('redoc_url', '/redoc') if enable_docs and os.getenv('redoc_url', '/redoc') else None
+    # set environment variable 'documentation' to 'True' if you want to enable the /docs path
+except TypeError:
+    enable_docs = False
+    docs_url = '/docs' if enable_docs else None
+    redoc_url = '/redoc' if enable_docs else None
 
 
-@app.get('/cors')
-async def handle(request: Request):
-    headers_ = request.query_params.get('headers',
-                                        "{\"User-Agent\": \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36}\"}")
-    headers_ = json.loads(headers_)
-    headers_[
-        "User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36}"
-    async with aiohttp.ClientSession(headers=multidict.CIMultiDict(headers_)) as session:
-        url = request.query_params.get('url')
-        url = url.replace("+", "%2B")
-        typr = request.query_params.get('origin', "normal")
-        if url.endswith('.m3u8'):
-            if typr == "aninin":
-                url += "#.mp4"
-            async with session.get(url) as resp:
-                headers = resp.headers.copy()
-                ret_head = resp.headers.copy()
-                headers['Access-Control-Allow-Origin'] = '*'
-                headers['Access-Control-Expose-Headers'] = 'Content-Disposition'
-                headers['Content-Disposition'] = 'attachment; filename="master.m3u8"'
-                del_keys = ['Vary', 'Server', 'Report-To', 'NEL', 'Content-Encoding', 'Transfer-Encoding',
-                            'Content-Length']
-                del_ret = ['Content-Type', 'Content-Encoding', 'Transfer-Encoding', 'Content-Length',
-                           'Server', 'Report-To', 'NEL']
-                for head in del_keys:
-                    try:
-                        del headers[head]
-                    except KeyError:
-                        pass
-                for head in del_ret:
-                    try:
-                        del ret_head[head]
-                    except KeyError:
-                        pass
-                text = await resp.text()
-                base_url = '/'.join(url.split('/')[:-1]) + '/'
-                modified_text = ''
-                if ("404 not" not in text) and ("404 Not" not in text):
-                    for line in text.split('\n'):
-                        line = line.strip(" ").strip('\n')
-                        if not line.strip(" ").strip('\n'):
-                            continue
-                        if line == "":
-                            continue
-                        if line.startswith('#'):
-                            modified_text += line + '\n'
-                            continue
-                        line = line.replace("+", "%2B")
-                        if ".m3u8" in line:
-                            if "http" not in line:
-                                line = base_url + line
-                            modified_text += '/cors?url=' + line + '&headers=' + json.dumps(dict(ret_head)) + '\n'
-                        else:
-                            if ('http://' not in url) or ('https://' not in url):
-                                line = base_url + line
-                            line = NEW_ID(line)
-                            modified_text += '/token?url=' + line + '&headers=' + json.dumps(dict(ret_head)) + '\n'
-                else:
-                    modified_text = text
-                if len(list(TOKENS.keys())) > 10000:
-                    del TOKENS[list(TOKENS.keys())[1]]
-                print(modified_text)
-                return Response(content=modified_text, headers=headers)
-        else:
-            async with session.get(url) as resp:
-                headers = resp.headers.copy()
-                headers['Access-Control-Allow-Origin'] = '*'
-                del_head = ['Vary', 'Server', 'Report-To', 'NEL', 'Transfer-Encoding', 'Content-Encoding',
-                            'Content-Length']
-                for key in del_head:
-                    try:
-                        del headers[key]
-                    except KeyError:
-                        pass
-                data = await resp.read()
-            return Response(content=data, headers=headers)
+app = FastAPI(openapi_url=None, docs_url=docs_url, redoc_url=redoc_url)
+default_port = "5010"
+
+if enable_docs:
+    @app.get('/')
+    async def home(_: Request):
+        return RedirectResponse('/docs')
 
 
-@app.get('/token')
-async def token_response(request: Request):
-    headers_ = request.query_params.get('headers',
-                                        "{\"User-Agent\": \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36}\"}")
-    headers_ = json.loads(headers_)
-    headers_[
-        "User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36}"
-    headers = {'Access-Control-Allow-Origin': '*'}
-    async with aiohttp.ClientSession(headers=multidict.CIMultiDict(headers_)) as session:
-        url = request.query_params.get('url')
-        url = TOKENS[url]
-        if url:
-            async with session.get(url) as resp:
-                headers = resp.headers.copy()
-                headers['Access-Control-Allow-Origin'] = '*'
-                del_head = ['Vary', 'Server', 'Report-To', 'NEL', 'Transfer-Encoding', 'Content-Encoding',
-                            'Content-Length']
-                for key in del_head:
-                    try:
-                        del headers[key]
-                    except KeyError:
-                        pass
-                data = await resp.read()
-    return Response(content=data, headers=headers)
+allowed_origins = os.getenv("origins", "*")
+# You may set your environment variable with the domains you want to allow requests from(your site)
+# You may put ',' between the domains if you have multiple domains
 
+try:
+    port = int(float(os.getenv("port", default_port)))
+except TypeError:
+    port = int(default_port)
+# You don't need to change anything here unless you want to run it on a different or specific port
+# to run on different port you can set the port env variable
 
-app.add_api_route('/cors', handle, methods=['GET'])
-app.add_api_route('/', home, methods=['GET'])
-app.add_api_route('/token', token_response, methods=['GET'])
+add_cors(app, allowed_origins)
+
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=5010)
+    uvicorn.run(app, host="0.0.0.0", port=port)
